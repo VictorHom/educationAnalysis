0f928469c8252db4627afd257df944798b7e7b7e
Victor Hom <vh62@cornell.edu>
2016-04-23 23:53:36 -0400

    add in mapbox

--- browser/js/app.js
+++ browser/js/app.js
@@ -2,6 +2,15 @@
 import '../style.scss';
 import React from 'react';
 import ReactDom from 'react-dom';
+const Mapbox = require('mapbox');
+
+(function(){
+  L.mapbox.accessToken = 'pk.eyJ1IjoidmljdG9yaG9tIiwiYSI6ImNpanhubDJzajE1eG52Z2x6M3Foa2xndWwifQ.JgTA8hpRA2Dq_m6wSPAW2A';
+  var map = L.mapbox.map('map', 'mapbox.streets', {
+    zoomControl: false
+  });
+  L.control.zoomslider().addTo(map);
+})()
 
 var Test = React.createClass({
 	render() {
